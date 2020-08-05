6ff75d7f76350d9dfad327900bd457829177bfa4
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-08-05 20:31:29 +0000

    My first Component

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weimar',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
