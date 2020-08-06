a09e2ff97976e56a72e487ffea9868f4383fb549
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-08-06 17:59:54 +0000

    Angular Configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,4 +18,82 @@ export class AppComponent implements OnInit {
 
     aux.subscribe((s:any) => console.log('event: ', s.clientX + ', ' + s.clientY));
   }
+
+  /*
+  ngOnInit() {
+    console.log('Working');
+
+    const aux = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    const auxSort = [5, 4, 3, 2, 1];
+
+    const index = aux.findIndex(s => s ===3);
+
+    console.log(index);
+
+    const aux2 = aux.filter(s => s % 2 === 0);
+
+    console.log('pares: ', aux2);
+
+    const aux3 = aux.map(s => s * 2);
+
+    console.log('map: ', aux3);
+
+    console.log('sort: ', auxSort.sort());
+
+    // Spread Operator
+    const aux5 = [1000, 2000, ...aux];
+
+    console.log('spread: ', aux5);
+
+    const aux6 = {
+      name: 'Weimar',
+      lastName: 'Torres'
+    };
+
+    const aux7 = {
+      address: 'La paz',
+      ...aux6
+    }
+
+    console.log('spread 2: ', aux7);
+
+    // Destructuring
+
+    const persona = {
+      data: {
+        name: 'Weimar'
+      },
+      address: {
+        n: 1,
+        extra: 'extra'
+      }
+    };
+
+    // const data = persona.data;
+    // const address = persona.address;
+
+    const { data, address } = persona;
+
+    console.log('data: ', data);
+    console.log('address: ', address);
+
+    // let
+
+    for(let index = 0; index < aux.length; index++) {
+      console.log('index: ', index);
+    };
+
+    // if
+
+    if (2 < 3) {
+      console.log('YES');
+    } else {
+      console.log('NO');
+    }
+
+    (3 < 2) ? console.log('YES') : console.log('NO');
+    
+  }
+  */
 }
