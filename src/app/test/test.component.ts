48aabbf7e9718501db0836d8e6439f8f1d73f56a
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-08-10 19:41:00 +0000

    Component life cycle

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,12 +1,49 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-weimar',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss']
 })
-export class TestComponent implements OnChanges, OnInit {
-  
+export class TestComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  @Input() name: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log("On Init");
+  }
+
+  ngOnChanges() {
+    console.log("On Changes");
+  }
+
+  ngDoCheck() {
+    console.log("Do Check");
+  }
+
+  ngAfterContentInit() {
+    console.log("After Content Init");
+  }
+
+  ngAfterContentChecked() {
+    console.log("After Content Checked");
+  }
+
+  ngAfterViewInit() {
+    console.log("After View Init");
+  }
+
+  ngAfterViewChecked() {
+    console.log("After View Checked");
+  }
+
+  ngOnDestroy() {
+    console.log("On Destroy");
+  }
+
+  /*
   nameTest: string
 
   @Input() age: number;
@@ -27,5 +64,6 @@ export class TestComponent implements OnChanges, OnInit {
   onClickSave() {
     this.clickAge.emit('Hiciste click');
   }
+  */
 
 }
