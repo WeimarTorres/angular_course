543e3884059b338e8535e23be1471398d206fe02
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-08-17 15:20:53 -0400

    First directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TestPipe } from './pipes/test.pipe';
 import { ExpoPipe } from './pipes/expo.pipe';
 import { PurePipe } from './pipes/pure.pipe';
 import { ImpurePipe } from './pipes/impure.pipe';
+import { NgColorDirective } from './directives/ng-color.directive';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ImpurePipe } from './pipes/impure.pipe';
     TestPipe,
     ExpoPipe,
     PurePipe,
-    ImpurePipe
+    ImpurePipe,
+    NgColorDirective
   ],
   imports: [
     BrowserModule,
