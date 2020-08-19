12711fc5f6f1a9ca07f6677533807574740f74b4
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-08-19 15:26:06 -0400

    Add data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'curso-angular';
+
+  data = [
+    {
+      nombre: "juan",
+      edad: 13,
+      grado: "p"
+    },
+    {
+      nombre: "marco",
+      edad: 15,
+      grado: "S"
+    },
+    {
+      nombre: "maria",
+      edad: 20,
+      grado: "P"
+    },
+    {
+      nombre: "marta",
+      edad: 22,
+      grado: "S"
+    },
+    {
+      nombre: "omar",
+      edad: 18,
+      grado: "P"
+    },
+    {
+      nombre: "miriam",
+      edad: 16,
+      grado: "S"
+    },
+    {
+      nombre: "roger",
+      edad: 18,
+      grado: "P"
+    },
+    {
+      nombre: "julieta",
+      edad: 20,
+      grado: "S"
+    },
+    {
+      nombre: "eber",
+      edad: 22,
+      grado: "P"
+    },
+    {
+      nombre: "juana",
+      edad: 25,
+      grado: "S"
+    }
+  ]
 }
