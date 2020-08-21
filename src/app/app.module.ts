2f9456048a8d955a8fd1b7b2e39d7923624fe62b
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-08-21 13:48:26 -0400

    First routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { AboutComponent } from './components/about/about.component';
+import { AppRouting } from './AppRouting';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
