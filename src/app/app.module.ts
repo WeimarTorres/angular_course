7ebc6723b4fd598aaa31c1bdff115657edebaab7
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-08-21 12:47:44 -0400

    Second partial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,40 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './components/test.component';
-import { Test2Component } from './components/test2/test2.component';
-import { ComponentE1Component } from './components/component-e1/component-e1.component';
-import { PersonModule } from './modules/person/person.module';
-import { ProductModule } from './modules/product/product.module';
-import { AdminModule } from './modules/admin/admin.module';
-import { UserModule } from './modules/user/user.module';
-import { TestPipe } from './pipes/test.pipe';
-import { ExpoPipe } from './pipes/expo.pipe';
-import { PurePipe } from './pipes/pure.pipe';
-import { ImpurePipe } from './pipes/impure.pipe';
-import { NgColorDirective } from './directives/ng-color.directive';
-import { NgFocusDirective } from './directives/ng-focus.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent,
-    Test2Component,
-    ComponentE1Component,
-    TestPipe,
-    ExpoPipe,
-    PurePipe,
-    ImpurePipe,
-    NgColorDirective,
-    NgFocusDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    PersonModule,
-    ProductModule,
-    AdminModule,
-    UserModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
