b492fd5917c2fb93566e70a29a614ac9ce8140c7
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-08-27 19:00:27 +0000

    Template container content

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
+import { TestComponent } from './login/test/test.component';
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, TestComponent],
   imports: [
     CommonModule,
     LoginRoutingModule
