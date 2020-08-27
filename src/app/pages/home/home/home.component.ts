f0c7f5fcf5817b29d47dcc48a7b2fa1797d12fe3
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-08-27 18:07:28 +0000

    Card angular material in home module

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products = [
+    {
+      description: 'description',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'Salteñas'
+    },
+    {
+      description: 'description',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'Salteñas'
+    },
+    {
+      description: 'description',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'Salteñas'
+    },
+    {
+      description: 'description',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'Salteñas'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
