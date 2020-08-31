b024e394809b13a22827deb920e71af8f9aabfc2
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-08-31 15:36:16 -0400

    Template-Driven Form

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -7,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = '';
+  password = '';
+
   sw = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(): void {
+    console.log('USERNAME: ', this.username);
+    console.log('PASSWORD: ', this.password);
+  }
+
+  onLogin2(form): void {
+    console.log('VARIABLE LOCAL FORM: ', form.value);
   }
 
 }
