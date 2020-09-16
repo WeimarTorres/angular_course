d7ac338210f71f681c411c4bb73464a1a699b9ba
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-09-16 14:28:01 -0400

    Fixed v.1

--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -1,3 +1,4 @@
+/*
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
@@ -14,17 +15,6 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = this.authService.getToken();
-    
-    /*
-    if(token) {
-      request = request.clone({
-        url: request.url + '?auth=' + token,
-        setHeaders: {
-          Authorization: 'Bearer ' + token
-        }
-      })
-    }
-    */
     
     if(token) {
       request = request.clone({
@@ -45,6 +35,71 @@ export class AuthInterceptor implements HttpInterceptor {
   private handle401Error(): Observable<any> {}
     this.authService.logout();
     return throwError('Error 401');
+  }
+
+}
+*/
+
+ /*
+    if(token) {
+      request = request.clone({
+        url: request.url + '?auth=' + token,
+        setHeaders: {
+          Authorization: 'Bearer ' + token
+        }
+      })
+    }
+    */
+
+import {Injectable} from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor, HttpErrorResponse
+} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {AuthService} from '../services/auth.service';
+import {catchError} from 'rxjs/operators';
+import {Router} from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService,
+              private router: Router) {
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = this.authService.getToken();
+
+    if (token) {
+      request = request.clone({
+        url: request.url.indexOf('?') > -1 ? `${request.url}&auth=${token}` : `${request.url}?auth=${token}`
+      });
+    }
+
+    /*if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + token
+        }
+      });
+    }*/
+
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          this.handle401Error();
+        }
+        return throwError('Error');
+      })
+    );
+  }
+
+  private handle401Error(): Observable<any> {
+    this.authService.logout();
+    return throwError('ERROR 401');
   }
 
 }
