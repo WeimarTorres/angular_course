e9bea977590729cdce45961423f59faa680550e8
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-09-17 16:09:55 -0400

    Exercise 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthService } from './shared/services/auth.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
 
 const routes: Routes = [
   {
@@ -33,7 +34,14 @@ const routes: Routes = [
     HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
